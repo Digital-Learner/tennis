1104b09bd73b0f21ad2e62e8272bb2a1195d4c90
Digital Learner <digital.learning.journeys@gmail.com>
2019-10-29 23:23:58 +0000

    Refactor #initialize

--- lib/game.rb
+++ lib/game.rb
@@ -1,34 +1,11 @@
 
 
 class Game
-  attr_reader :server, :receiver, :score, :points
+  attr_reader :server, :receiver, :points
 
   def initialize(server, receiver)
     @server = server
     @receiver = receiver
-    @score = { server => "love", receiver => "love" }
-    @points = 0
-  end
-
-  def point_to(player)
-    case @score[player]
-    when "love"
-      @score[player] = "15"
-    when "15"
-      @score[player] = "30"
-    when "30"
-      @score[player] = "40"
-    when "40"
-      # duce? ? @score[player] = ":
-    end
-    @points += 1
-    @score
-  end
-
-  private
-
-  def duce?
-    return @score[server] == @score[receiver] if @points > 2
-    false
+    @points = { server => 0, receiver => 0 }
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,21 +2,13 @@ require './lib/game'
 
 RSpec.describe "Game" do
   describe "#initialize" do
-    it "score is love all" do
-      server = "j"
-      receiver = "b"
+    it "points are 0 (server) - 0 (receiver)" do
+      server = "J"
+      receiver = "B"
       game = Game.new(server, receiver)
-      expect(game.score).to eql( { server => "love", receiver => "love"} ) 
-    end
-  end
-
-  describe "Player 1 wins first point" do
-    it "has score 15:love" do
-      server = "j"
-      receiver = "b"
-      game = Game.new(server, receiver)
-      game.point_to(server)
-      expect(game.score).to eql( { server => "15", receiver => "love"} )
+      expect(game.points).to be_a(Hash)
+      expect(game.points[server]).to eq 0
+      expect(game.points[receiver]).to eq 0
     end
   end
 end
