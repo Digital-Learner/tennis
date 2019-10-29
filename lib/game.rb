4684a6c54d9c30bddc665fa8e136fc846bdf8621
Digital Learner <digital.learning.journeys@gmail.com>
2019-10-29 23:52:29 +0000

    Add rubocop gem
     - for linting of code
     - fix missing frozen_string_literal offense

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
 
-
+# A tennis match has a number of games.
+# The purpose is to correctly calculate an individual games score
 class Game
   attr_reader :server, :receiver, :points
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require './lib/game'
 
-RSpec.describe "Game" do
-  describe "#initialize" do
-    it "points are 0 (server) - 0 (receiver)" do
-      server = "J"
-      receiver = "B"
+RSpec.describe 'Game' do
+  describe '#initialize' do
+    it 'points are 0 (server) - 0 (receiver)' do
+      server = 'J'
+      receiver = 'B'
       game = Game.new(server, receiver)
       expect(game.points).to be_a(Hash)
       expect(game.points[server]).to eq 0
