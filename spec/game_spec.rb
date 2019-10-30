81e2650d464f4979eabc9ca7e66cb777bc25fb35
Digital Learner <digital.learning.journeys@gmail.com>
2019-10-30 00:31:01 +0000

    Refactor the spec
     - to use Game class instead of 'Game'
     - DRY up spec using let for all tests to remove duplication

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,21 +2,22 @@
 
 require './lib/game'
 
-RSpec.describe 'Game' do
+RSpec.describe Game do
+  let(:server) { 'J' }
+  let(:receiver) { 'B' }
+  subject { described_class.new(server, receiver) }
+
   describe '#initialize' do
     it 'points are 0 (server) - 0 (receiver)' do
-      server = 'J'
-      receiver = 'B'
-      game = Game.new(server, receiver)
-      expect(game.points).to be_a(Hash)
-      expect(game.points[server]).to eq 0
-      expect(game.points[receiver]).to eq 0
+      expect(subject.points).to be_a(Hash)
+      expect(subject.points[server]).to eq 0
+      expect(subject.points[receiver]).to eq 0
     end
   end
 
   describe '#score' do
     context 'for a new game before a point is played' do
-      subject { Game.new('J', 'B').score }
+      subject { described_class.new(server, receiver).score }
       it { is_expected.to equal 'Love-all' }
     end
   end
